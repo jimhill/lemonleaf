cb2a930f4aea2f1a6d2bf06fc6848aca5acac9ed
Wiseguy Digital <studio@wiseguydigital.com>
2014-08-15 15:47:42 +0100

    Added main.js

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+/* ========================================================================
+ * Built using LemonLeaf
+ *
+ * A quickstart environment for LemonStand theme development 
+ * https://github.com/jimhill/lemonleaf
+ * ========================================================================
+ * Licensed under MIT (https://github.com/jimhill/lemonleaf/blob/master/LICENSE)
+ * ======================================================================== */
